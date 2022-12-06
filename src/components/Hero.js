cb1dc3d3ded3ff590ae3cbfad8e680f8366560c1
matteopelusi7 <pelusimatteo02@gmail.com>
2022-12-06 16:58:13 +0100

    add hero and about screen

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,10 +1,23 @@
 import React from "react";
 import backImage from "../assets/image/home-hero.jpg";
 
-const Hero = () => {
+const Hero = ({children, img, disableOverlay}) => {
+
+  const image = img ? img : backImage
+
   return( 
-    <div>
-        Hero
+    <div 
+      className="hero-img-container"
+      style={{
+        background: `url(${image})`,
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover'
+      }}
+    >
+      <div className={`${!disableOverlay ? 'hero-overlay' : ''}`}>
+        <div className="container hero-container">{children}</div>
+      </div>
     </div>
   );
 };
