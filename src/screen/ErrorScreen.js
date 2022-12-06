223251d7dfc8791a78a8095308c90304cc3a0510
matteopelusi7 <pelusimatteo02@gmail.com>
2022-12-06 15:45:21 +0100

    add error screen

--- src/screen/ErrorScreen.js
+++ src/screen/ErrorScreen.js
@@ -1,13 +1,29 @@
 import React from "react";
 import styled from "styled-components";
-// import Lottie from "react-lottie";
+import Lottie from "react-lottie";
 import animationData from "../assets/animation/not-found.json";
 import { Link } from "react-router-dom";
 
 const ErrorScreen = () => {
   return(
     <Wrapper>
-        Error Scree
+      <h3>Pagina Non Trovata</h3>
+      <Lottie 
+        options= {{
+          loop: true,
+          autoplay: true,
+          reverse: true,
+          animationData: animationData,
+          renderSettings: {
+            preserveAspectRatio: 'xMidYMid slice'
+          },
+        }}
+        width={500}
+        height={500} 
+      />
+      <Link to='/' className="btn btn-primary">
+        Torna in Home
+      </Link>
     </Wrapper>
   );
 };
@@ -15,9 +31,15 @@ const ErrorScreen = () => {
 const Wrapper = styled.section`
   min-height: 86.2vh;
   display: grid;
+  display: --ms-grid;
+  display: --moz-grid;
+  display: --webkit-grid;
   place-items: center;
   h3 {
     text-transform: uppercase;
+  }
+  .btn {
+    border-radius: 22px;
   }
   @media screen and (min-width: 992px) {
     min-height: 84.5vh;
