c1de1d4e68f9141889a5b46a8eaa2e86f7e93261
matteopelusi7 <pelusimatteo02@gmail.com>
2022-12-07 11:01:38 +0100

    add form

--- src/screen/ContactScreen.js
+++ src/screen/ContactScreen.js
@@ -5,7 +5,46 @@ import backImage from "../assets/image/Contact-hero.png";
 const ContactScreen = () => {
   return( 
     <>
-        ContactScreen
+      <Hero img={backImage} disableOverlay>
+        <div className="contact-hero container">
+          <div className="contact-hero-text">
+            <div className="contact-hero-title">
+              <h2 className="contact-title">Vorresti aggiungere un Cocktail?</h2>
+              <h4 className="contact-subtitle">Il nostro team è sempre disponibile per valutare possibili nuove ricette ed ad aggiungerle alla nostro database</h4>
+            </div>
+          </div>
+          <div className="contact-form-container container">
+            <form action="https://formspree.io/f/xpznvlzd" method="POST" className="contact-form container">
+              <div className="form-group">
+                <label htmlFor="nome">Nome</label>
+                <input type="text" id="nome" name="name" className="input" placeholder="Daniele" />
+                <hr />
+              </div>
+              <div className="form-group">
+                <label htmlFor="cognome">Cognome</label>
+                <input type="text" id="cognome" name="cognome" className="input" placeholder="Tamarindo" />
+                <hr />
+              </div>
+              <div className="form-group">
+                <label htmlFor="email">Email</label>
+                <input type="email" id="email" name="email" className="input" placeholder="daniele@esempio.com" />
+                <hr />
+              </div>
+              <div className="form-group">
+                <label htmlFor="cellulare">Telefono</label>
+                <input type="cel" id="cellulare" name="cellulare" className="input" placeholder="555 555-5555" />
+                <hr />
+              </div>
+              <div className="form-group">
+                <label htmlFor="ricetta">La tua ricetta</label>
+                <input type="tel" id="ricetta" name="ricetta" className="input" placeholder="Descrivi la tua ricetta" />
+                <hr />
+              </div>
+              <button type="submit" className="btn btn-primary">Invia Ricetta</button>
+            </form>
+          </div>
+        </div>
+      </Hero>
     </>
   );
 };
